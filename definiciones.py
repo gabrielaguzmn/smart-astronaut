6d7bc92296d45ed0df932531307ba52fa33e9573
David Guerrero <dsgd2005@gmail.com>
2025-09-27 11:10:55 -0500

    Modified cost type

--- definiciones.py
+++ definiciones.py
@@ -17,7 +17,7 @@ class Nodo:
     """
     def __init__(self, padre: "Nodo", ubicacion: tuple, mapa: list[list[int]],
                  nave: tuple[bool, int], muestras: int, 
-                 costo: int = None, heuristica = None):
+                 costo: bool = None, heuristica = None):
         # Para el estado del nodo
         self.padre = padre
         self.ubicacion = ubicacion
